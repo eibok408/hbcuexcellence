079573525ac6e5c052f20d449be30745ba983676
Antonio McMichael <antonio_mcmichael@hotmail.com>
2018-06-01 18:51:58 -0700

    Users/anmcmi/add recaptcha (#1)
    * Adding recaptcha server side validation.
    
    * Additional code changes.

--- hbcu-excellence/app/controllers/Application.java
+++ hbcu-excellence/app/controllers/Application.java
@@ -1,12 +1,13 @@
 package controllers;
 
-import play.*;
-import play.data.validation.Valid;
-import play.mvc.*;
-import utils.Contact;
-import utils.UploadedFile;
-
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
@@ -14,10 +15,17 @@ import com.google.appengine.api.taskqueue.RetryOptions;
 import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.gson.Gson;
 
-import models.*;
+import models.ScholarshipApplication;
+import play.Play;
+import play.data.validation.Valid;
+import play.mvc.Controller;
+import utils.Contact;
+import utils.RecaptchaResponseVerification;
+import utils.UploadedFile;
 
 public class Application extends Controller
 {
+    private static final String RecaptchaVerificationUrl = "https://www.google.com/recaptcha/api/siteverify";
 
     public static void index()
     {
@@ -27,8 +35,8 @@ public class Application extends Controller
     {
         render();
     }
-	
-	public static void aboutHbcus()
+
+    public static void aboutHbcus()
     {
         render();
     }
@@ -37,68 +45,133 @@ public class Application extends Controller
     {
         render();
     }
-    
+
     public static void scholarshipApplication()
     {
         render();
     }
-    
+
     public static void scholarshipApplicationPost(@Valid ScholarshipApplication submission)
     {
-    	if (validation.hasErrors())
-    	{
-    		render("@scholarshipApplication");
-    	}
-    	
-    	// If the form passes validation set the value of the time stamp immediately.
-    	submission.timeStamp = new Date();
-    	
-    	// Extract all upload files from the HTTP POST parameters and add them to a list of uploaded files.
-    	List<UploadedFile> uploads = new ArrayList<UploadedFile>();
-    	uploads.add(new UploadedFile("Transcript", params.get("transcript", byte[].class), params.get("transcriptFileName")));
-    	uploads.add(new UploadedFile("Resume", params.get("resume", byte[].class), params.get("resumeFileName")));
-    	uploads.add(new UploadedFile("LetterOfRecommendation", params.get("letterOfRecommendation", byte[].class), params.get("letterOfRecommendationFileName")));
-    	uploads.add(new UploadedFile("Photo", params.get("photo", byte[].class), params.get("photoFileName")));
-    	uploads.add(new UploadedFile("Application", submission.generateReport(), "application.pdf"));
-    	
-    	// Create a folder for storing application materials.
-    	String parentFolderName = submission.firstname + " " + submission.lastname;
-    	submission.googleDriveFolderID = Utilities.createGoogleDriveFolder(parentFolderName);
-		
-		// Upload all files associated with the application to the Google drive folder.
-		HashMap<String, String> uploadedFileIDs = Utilities.uploadFilesToGoogleDrive(uploads, submission.googleDriveFolderID, parentFolderName);
-		submission.uploadedFileGoogleDriveIDs = new Gson().toJson(uploadedFileIDs);
-		submission.insert();
-    	
-    	scholarshipApplicationConfirmation();
+        if (validation.hasErrors())
+        {
+            render("@scholarshipApplication");
+            return;
+        }
+
+        // If the form passes validation set the value of the time stamp immediately.
+        submission.timeStamp = new Date();
+
+        // Extract all upload files from the HTTP POST parameters and add them to a list of uploaded files.
+        List<UploadedFile> uploads = new ArrayList<UploadedFile>();
+        uploads.add(new UploadedFile("Transcript", params.get("transcript", byte[].class), params.get("transcriptFileName")));
+        uploads.add(new UploadedFile("Resume", params.get("resume", byte[].class), params.get("resumeFileName")));
+        uploads.add(new UploadedFile("LetterOfRecommendation", params.get("letterOfRecommendation", byte[].class), params.get("letterOfRecommendationFileName")));
+        uploads.add(new UploadedFile("Photo", params.get("photo", byte[].class), params.get("photoFileName")));
+        uploads.add(new UploadedFile("Application", submission.generateReport(), "application.pdf"));
+
+        // Create a folder for storing application materials.
+        String parentFolderName = submission.firstname + " " + submission.lastname;
+        submission.googleDriveFolderID = Utilities.createGoogleDriveFolder(parentFolderName);
+
+        // Upload all files associated with the application to the Google drive folder.
+        HashMap<String, String> uploadedFileIDs = Utilities.uploadFilesToGoogleDrive(uploads, submission.googleDriveFolderID, parentFolderName);
+        submission.uploadedFileGoogleDriveIDs = new Gson().toJson(uploadedFileIDs);
+        submission.insert();
+
+        scholarshipApplicationConfirmation();
     }
-    
+
     public static void scholarshipApplicationConfirmation()
     {
         render();
     }
-    
+
     public static void contact(String successMessage)
     {
-    	render(successMessage);
+        render(successMessage);
     }
-    
+
     public static void contactPost(@Valid Contact contact)
     {
-    	if(validation.hasErrors())
-    	{
-    		render("@contact");
-    	}
-    	
-    	Queue queue = QueueFactory.getDefaultQueue();
-		TaskOptions taskOptions = TaskOptions.Builder.withUrl("/Utilities/sendContactEmail");					
-		taskOptions.param("contact.name", contact.name);
-		taskOptions.param("contact.email", contact.email);
-		taskOptions.param("contact.message", contact.message);
-		taskOptions.countdownMillis(10 * 1000);
-		taskOptions.retryOptions(RetryOptions.Builder.withTaskRetryLimit(0));
-		queue.add(taskOptions);
-    	
-    	contact("Contact request has been sent.");
+        if (validation.hasErrors())
+        {
+            render("@contact");
+            return;
+        }
+
+        if (!validateRecaptcha(params.get("g-recaptcha-response"), request.remoteAddress))
+        {
+            render("@contact");
+            return;
+        }
+
+        Queue queue = QueueFactory.getDefaultQueue();
+        TaskOptions taskOptions = TaskOptions.Builder.withUrl("/Utilities/sendContactEmail");
+        taskOptions.param("contact.name", contact.name);
+        taskOptions.param("contact.email", contact.email);
+        taskOptions.param("contact.message", contact.message);
+        taskOptions.countdownMillis(10 * 1000);
+        taskOptions.retryOptions(RetryOptions.Builder.withTaskRetryLimit(0));
+        queue.add(taskOptions);
+
+        contact("Contact request has been sent.");
+    }
+
+    private static boolean validateRecaptcha(String recaptchaResponse, String ipAddress)
+    {
+        if (recaptchaResponse == null || recaptchaResponse.isEmpty())
+        {
+            return false;
+        }
+
+        if (ipAddress == null || ipAddress.isEmpty())
+        {
+            return false;
+        }
+
+        try
+        {
+            // Call ReCaptcha API.
+            URL requestUrl = new URL(String.format(
+                "%s?secret=%s&response=%s&remoteip=%s",
+                RecaptchaVerificationUrl,
+                Play.configuration.getProperty("recaptcha.secret"),
+                recaptchaResponse,
+                ipAddress));
+
+            HttpURLConnection httpURLConnection = (HttpURLConnection)requestUrl.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("Content-Length", "0");
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.connect();
+
+            // Verify the request was successful.
+            int responseCode = httpURLConnection.getResponseCode();
+            if (responseCode >= 400)
+            {
+                return false;
+            }
+
+            // Read the response body.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                stringBuilder.append(line);
+            }
+
+            // Deserialize the response body.
+            Gson gson = new Gson();
+            RecaptchaResponseVerification recaptchaResponseVerification = gson.fromJson(stringBuilder.toString(), RecaptchaResponseVerification.class);
+            return recaptchaResponseVerification.success;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 }
---
+++ hbcu-excellence/app/utils/RecaptchaResponseVerification.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.util.Date;
+
+public class RecaptchaResponseVerification
+{
+    public boolean success;
+    public Date challenge_ts;
+    public String hostname;
+}
